b8599aeb7d9fc29cc7eca2482e90efd5ce8a6059
Matt Filetto <matt.filetto@gmail.com>
2024-02-16 14:33:36 -0800

    replace alpha -> sigma
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/android/settings/sound/CustomVibrationPreferenceController.java
+++ src/com/android/settings/sound/CustomVibrationPreferenceController.java
@@ -34,7 +34,7 @@ import com.android.settings.sound.CustomVibrationPreferenceController;
 import com.android.settings.Utils;
 import com.android.settingslib.core.AbstractPreferenceController;
 
-import com.alpha.settings.preferences.CustomSeekBarPreference;
+import com.sigma.settings.preferences.CustomSeekBarPreference;
 
 /**
  * This class allows choosing a vibration pattern while ringing
